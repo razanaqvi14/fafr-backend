bc310df11dfa1e068580bae17041e389f3f88c58
razanaqvi <razanaqvi2004@gmail.com>
2024-11-14 00:25:29 +0500

    updated app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from pymongo import MongoClient
 
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": ["http://localhost:3000"]}})
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 load_dotenv()
 
